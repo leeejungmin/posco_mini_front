52b5891d708e490107f26bd29902f4f87b1abe52
leeejungmin <ljm3453@gmail.com>
2022-06-20 01:34:27 +0900

    jung

--- front/src/Sagas/user.js
+++ front/src/Sagas/user.js
@@ -25,9 +25,9 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
 
-export const loginRequestAction = (data) => {
+export const loginRequestdata = (data) => {
   // 로그인 요청하기
-  console.log("reducer / loginRequestAction");
+  console.log("reducer / loginRequestdata");
   console.log(data); 
   return {
     type: LOGIN_REQUEST,
@@ -59,38 +59,52 @@ export const getUserById = async (users, id) => {
 const loginSlice = createSlice({
   name: "loginPost",
   initialState,
-  reducers: {
-      
-      LOGIN_REQUEST: (state) => {
-        return {
-          ...state,
-          isLoading : true,
-          //data : state.data,
-        }
-      },
-
-      LOGIN_SUCCESS: (state, action) => {
+  reducers: {},
+    extraReducers: (builder) => {
+      builder
+      .addCase(LOGIN_SUCCESS, (state, { data }) => {
         console.log("login success slice...");
-        localStorage.setItem("id", action.user.id);
-        localStorage.setItem("token", action.token);
-      return {
-          ...state,
-          isLogin: action.isLogin, //
-          me: action.user,
-          myId: action.user.id,
-      };
-      },
-
-      LOGIN_CHECK: (state, action) => {
-      
+        localStorage.setItem("id", data.user.id);
+        localStorage.setItem("token", data.token);
         return {
           ...state,
-          isLogin: action.isLogin, 
-          me: action.user,
-          myId: action.user.id,
+          isLogin: data.isLogin, //
+          me: data.user,
+          myId: data.user.id,
       };
-      },
-
+      })
+      .addCase(LOGIN_CHECK, (state, { data }) => {
+        console.log("login check success slice...");
+        localStorage.setItem("id", data.user.id);
+        localStorage.setItem("token", data.token);
+        return {
+          ...state,
+          isLogin: data.isLogin, //
+          me: data.user,
+          myId: data.user.id,
+      };
+      })
+      .addCase(SELECT_USERLIST, (state, { data }) => {
+        console.log("login check selectuserlist slice...");
+        localStorage.setItem("id", data.user.id);
+        localStorage.setItem("token", data.token);
+        return {...state, me : data};
+      })
+      .addCase(DELETE_USER, (state, { data }) => {
+        console.log("login check delete slice...");
+        localStorage.setItem("id", data.user.id);
+        localStorage.setItem("token", data.token);
+        return {...state, me : data};
+      })
+      .addCase(COUNT_REVIEW,(state, {payload}) => {
+        console.log("count review : " + payload);
+        return {...state, count : payload};
+    })
+    .addCase(LOGOUT, (state, { payload }) => {
+      localStorage.removeItem("id");
+      localStorage.removeItem("token");
+      return { ...state, isLogin: false, me: {}, myId: "" };
+  })
   },
   
 });
--- front/src/Sagas/userApi.js
+++ front/src/Sagas/userApi.js
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import {customAxios} from "../Http/customAxios";
 import { useDispatch, useSelector } from "react-redux";
 import { delay, put, fork, all, takeLatest, takeEvery, call } from "redux-saga/effects";
-import { LOGIN_REQUEST, LOGIN_SUCCESS } from "./user";
+import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_CHECK } from "./user";
 
 
 function* Login(action) {
@@ -21,10 +21,35 @@ function* Login(action) {
     });
 }
 
+function* getUserById(users, id) {
+  // const findUserById = yield call( users.find((user) => user.id === id);
+  const { data } = yield call(customAxios, "get",`/user/me`, action.data);
+  return data;
+};
+
+function* deleteUserApi(){
+  const { status } = yield call( customAxios, "delete","/user/",null);
+if (status !== 201) {
+  throw new Error("error");
+}
+return status;
+
+};
+
+function* getcountReview (){
+  const countRes = yield call( customAxios,"get", "/review/count");
+  return countRes;
+}
+function* logoutApi (userId){
+    return true;
+};
 
 function* watchLogin() {
     console.log("saga  watchLogin........");
     yield takeLatest(LOGIN_REQUEST, Login);
+    yield takeLatest(LOGOUT, logoutApi);
+    yield takeLatest(SELECT_USERLIST, getUserById);
+    yield takeLatest(COUNT_REVIEW, getcountReview);
   }
   
   export default watchLogin;
